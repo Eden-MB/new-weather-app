00d99563968096d38299f331e562fd60c41621a4
Eden-MB <tutor.edenmillion@gmail.com>
2024-10-10 17:09:28 +0300

    Worked on App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,96 @@
-import React from "react";
+import React, {useState} from "react";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from "axios";
 
 
+
 export default function App() {
-  return (
+  const [city, setCity] = useState(" ")
+  const [weather, setWeather] = useState({ready: false});
+
+  const icons= <i class="fa fa-search" aria-hidden="true"></i>
+
+
+function handleSubmit(event) {
+event.preventDefault();
+let apiKey= "97bed167ec49bff56e6c1b63daef9c86";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric` 
+axios.get(apiUrl).then(weatherResponse);
+}
+function updateCity(event) {
+  event.preventDefault();
+  setCity(event.target.value)
+}
+
+function weatherResponse(response) {
+setWeather({
+  ready: true,
+  temperature: response.data.main.temp,
+  humidity: response.data.main.humidity,
+  wind: response.data.main.wind,
+  description: response.data.weather[0].description,
+  icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+
+})
+
+}
+
+if (weather.ready) {
+return (
   <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input type="search" placeholder="Enter a city..." autoFocus="on" />
-            </div>
-          <div className="col-3"> <input type="submit" className="btn btn-primary" /></div>
+  <form onSubmit={handleSubmit}>
+    <div className="row">
+      <div className="col-9">
+        <input type="search" placeholder="Enter a city..." autoFocus="on" onChange={updateCity}/>
         </div>
-        </form>
-     
-      <div className="row temperature">
-        <div className="col-2"><img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="" />
-        </div>
-        <div className=" col-2"><h2>21<span className="celcius">°C</span></h2></div>
-      <div className="col-7"> <h1 className="mt-4">Addis Ababa</h1></div>
-      </div>
-      <div className="row">
-        <div className="col-6">
-        <ul>
-        <li>Monday 6:21pm</li>
-        <li>Mostly cloudy</li>
-      </ul>
-        </div>
-      
-      <div className="col-6">
-        <ul>
-          <li>Percipitation 5%</li>
-          <li>Humidity 84%</li>
-          <li>Wind 5 km/h</li>
-        </ul>
-      </div>
-      </div>
-      <footer>This project was coded by <strong>Eden Million</strong> and is open-sourced on <strong>GitHub</strong></footer>
+        <div className="col-3"> <button className="btn-primary"> <i class="fa fa-search" aria-hidden="true"></i>
+        </button> </div>
+    </div>
+    </form>
+  <div className="row temperature">
+    <div className="col-2"><img src={weather.icon} alt="" />
+    </div>
+    <div className=" col-2"><h2>{Math.round(weather.temperature)}<span className="celcius">°C</span></h2></div>
+  <div className="col-7"> <h1 className="mt-4">{city}</h1></div>
   </div>
-  )
+  <div className="row">
+    <div className="col-6">
+    <ul>
+    <li>Monday 6:21pm</li>
+    <li>{weather.description}</li>
+  </ul>
+    </div>
+  
+  <div className="col-6">
+    <ul>
+      <li>Percipitation: 5%</li>
+      <li>Humidity: {weather.humidity}%</li>
+      <li>Wind: {weather.wind}km/h</li>
+    </ul>
+  </div>
+  </div>
+  <footer>This project was coded by <strong>Eden Million</strong> and is open-sourced on <strong>GitHub</strong></footer>
+</div>
+);
+
+} else {
+  return (
+    <div className="Weather">
+  <form onSubmit={handleSubmit}>
+    <div className="row">
+      <div className="col-9 icon" >
+        <input type="search" placeholder= " Enter a city..." autoFocus="on" onChange={updateCity} />
+        </div>
+        
+      <div className="col-3"> <button className="btn-primary"> <i class="fa fa-search" aria-hidden="true"></i>
+      </button></div>
+    </div>
+    </form>
+  
+    <footer>This project was coded by <strong>Eden Million</strong> and is open-sourced on <strong>GitHub</strong></footer>
+  </div>
+);
+}
 }
 
